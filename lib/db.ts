a8ed162b048175fef4a16c06e9d1fcee0ecf00d0
ss-manifold <113132215+ss-manifold@users.noreply.github.com>
2022-11-04 12:11:22 -0500

    .

---
+++ lib/db.ts
@@ -0,0 +1,7 @@
+export const DB: { [name: string]: string } = {
+    phosphenism:
+      "https://beta.catalog.works/_next/image?url=https%3A%2F%2Fstorageapi.fleek.co%2Fcatalogworks-team-bucket%2Fprod%2Fusers%2F0xea9109739cd6a0f36321e2e302df1319038b954b%2Fimages%2Fprofile_picture.png&w=256&q=30",
+    yunice:
+      "https://beta.catalog.works/_next/image?url=https%3A%2F%2Fstorageapi.fleek.co%2Fcatalogworks-team-bucket%2Fprod%2Fusers%2F0x06361a93e8d92409df2d15b0969690f02e51bcae%2Fimages%2Fprofile_picture.jpeg&w=256&q=30",
+  };
+  
---
+++ pages/api/og.tsx
@@ -0,0 +1,107 @@
+import { ImageResponse } from "@vercel/og";
+import { NextRequest } from "next/server";
+import { DB } from "../../lib/db";
+
+export const config = {
+  runtime: "experimental-edge",
+};
+
+export default function (req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+
+  // ?username=<username>
+  const hasUsername = searchParams.has("username");
+  const username = hasUsername
+    ? searchParams.get("username")?.slice(0, 100)
+    : undefined;
+
+  const img = username
+    ? DB[username]
+    : "https://beta.catalog.works/assets/icons/logo.svg";
+
+  return new ImageResponse(
+    (
+      <div
+        className=""
+        style={{
+          background: "black",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          textAlign: "center",
+          alignItems: "center",
+          justifyContent: "center",
+          color: "white",
+        }}
+      >
+        <div
+          style={{
+            fontSize: 128,
+            background: "red",
+            width: "100%",
+            height: "33.33%",
+            display: "flex",
+            textAlign: "center",
+            alignItems: "flex-end",
+            justifyContent: "center",
+          }}
+        >
+          <p></p>
+        </div>
+        <div
+          style={{
+            fontSize: 40,
+            width: "100%",
+            flexBasis: "33%",
+            height: "100%",
+            display: "flex",
+            textAlign: "center",
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+          }}
+        >
+          <div
+            style={{
+              fontSize: 40,
+              height: "100%",
+              display: "flex",
+              textAlign: "center",
+              alignItems: "center",
+              justifyContent: "center",
+              flexDirection: "column",
+              padding: "20px",
+            }}
+          >
+            <p>CATALOG</p>
+            <img
+              src={img}
+              style={{
+                height: "80%",
+                padding: "20px",
+                borderRadius: "50px",
+              }}
+            />
+          </div>
+        </div>
+        <div
+          style={{
+            fontSize: 128,
+            background: "green",
+            width: "100%",
+            height: "33.33%",
+            display: "flex",
+            textAlign: "center",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        />
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 600,
+    }
+  );
+}
